d001cc08e8febecec9c53ff573661135225c9a76
manjeetexe <manjeetsharma9257@gmail.com>
2025-01-04 11:56:51 +0530

    creating function deleted

--- Socia-media-analytics Backend/Models/PostModel.js
+++ Socia-media-analytics Backend/Models/PostModel.js
@@ -4,28 +4,7 @@ const db = require('./../DB_Connection');
 const collectionName = 'posts';
 
 // Ensure the collection exists
-const checkCollectionExistence = async () => {
-  try {
-    const collections = await db.listCollections();
 
-    if (!collections.includes(collectionName)) {
-      console.log(`Collection '${collectionName}' does not exist. Creating collection...`);
-      await db.createCollection(collectionName);
-    } else {
-      console.log(`Collection '${collectionName}' already exists. Skipping creation.`);
-    }
-  } catch (error) {
-    if (error.message.includes('CollectionAlreadyExistsError')) {
-      console.log(`Collection '${collectionName}' already exists.`);
-    } else {
-      console.error('Error checking or creating collection:', error);
-      process.exit(1);
-    }
-  }
-};
-
-// Call to check collection existence when the app starts
-checkCollectionExistence();
 
 module.exports = {
   collectionName,
